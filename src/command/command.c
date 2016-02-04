56bbfa12de00b9a2c0e8477e40f9e26e5492d932
MrPlatwnas <platonickola@hotmail.com>
2016-02-05 01:03:30 +0200

    Initial Commit

---
+++ src/command/command.c
@@ -0,0 +1,24 @@
+/******************************************************
+File implementation : command.h
+Authors             : P. N. Kiorpelidis & G. Koryllos
+Purpose             : command functions definitions
+Date                : 28-1-2015
+*******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void boardsize(unsigned int n_rows, unsigned int n_cols)
+{
+
+}
+
+void clear_board(int** grid)
+{
+
+}
+
+void walls(unsigned int n_walls)
+{
+  
+}
---
+++ src/command/command.h
@@ -0,0 +1,10 @@
+/******************************************************
+File implementation : command.h
+Authors             : P. N. Kiorpelidis & G. Koryllos
+Purpose             : command functions declarations
+Date                : 28-1-2015
+*******************************************************/
+
+void boardsize(unsigned int n_rows, unsigned int n_cols);
+void clear_board(int** grid);
+void walls(unsigned int n_walls);
