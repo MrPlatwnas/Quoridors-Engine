b43e797ec815fcf43ffdc1a05c16192d37369611
MrPlatwnas <platonickola@hotmail.com>
2016-02-05 01:02:31 +0200

    Initial Commit

---
+++ src/cli/main.c
@@ -0,0 +1,16 @@
+/******************************************************
+File implementation : main.c
+Authors             : P. N. Kiorpelidis & G. Koryllos
+Purpose             : User Interface
+Date                : 28-1-2015
+*******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../misc/misc.h"
+
+int main(int argc, char* argv[])
+{
+  
+}
