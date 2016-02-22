8279d2fc17c3a9624b2d1b7536c18c40408dc259
MrPlatwnas <platonickola@hotmail.com>
2016-02-22 03:52:42 +0200

    Added function prototypes, renamed variables and added #ifdef

--- src/misc/misc.h
+++ src/misc/misc.h
@@ -5,5 +5,10 @@ Purpose             : miscellaneous functions declarations
 Date                : 2-1-2015
 *******************************************************/
 
+#ifndef _MISC_H_
+#define _MISC_H_
 
-int** initGrid(unsigned int rows, unsigned columns, int** grid);
+int** initGrid(unsigned int n_rows, unsigned n_cols, int** grid);
+void zerofyGrid(unsigned int n_rows, unsigned int n_cols, int** grid);
+
+#endif
