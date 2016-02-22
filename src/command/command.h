45d588bf0a994ec2d4bdd7e981a36c7306ebfd29
MrPlatwnas <platonickola@hotmail.com>
2016-02-22 06:53:43 +0200

    Added name function prototype

--- src/command/command.h
+++ src/command/command.h
@@ -8,6 +8,7 @@ Date                : 28-1-2015
 #ifndef _COMMAND_H_
 #define _COMMAND_H_
 
+void name();
 void boardsize(unsigned int n_rows, unsigned int n_cols);
 void clear_board(int** grid);
 void walls(unsigned int n_walls);
