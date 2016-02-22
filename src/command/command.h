40ee44ecbd44cf162d26cf42b2678a58f7a4dd63
MrPlatwnas <platonickola@hotmail.com>
2016-02-22 06:50:33 +0200

    Added #ifdef structure for controlled inclusion

--- src/command/command.h
+++ src/command/command.h
@@ -5,6 +5,11 @@ Purpose             : command functions declarations
 Date                : 28-1-2015
 *******************************************************/
 
+#ifndef _COMMAND_H_
+#define _COMMAND_H_
+
 void boardsize(unsigned int n_rows, unsigned int n_cols);
 void clear_board(int** grid);
 void walls(unsigned int n_walls);
+
+#endif
