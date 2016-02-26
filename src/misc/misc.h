51983bc928027ddbaf687d33f1b2ed7f5c2e4f1b
MrPlatwnas <platonickola@hotmail.com>
2016-02-27 00:14:12 +0200

    Changed copyStringUntil function declaration comment

--- src/misc/misc.h
+++ src/misc/misc.h
@@ -20,7 +20,7 @@ void print2DArrayAsInt(unsigned int n_rows, unsigned int n_cols, int** array);
 //for a given 2D array with n_rows rows and n_cols columns, it's content is printed as characters.
 void print2DArrayAsChar(unsigned int n_rows, unsigned int n_cols, int** array);
 
-//copies sourceStr content to destinationStr until the character endChar is found. If character endChar does not exist inside sourceStr then everything is copied over.
+//copies sourceStr content to destinationStr until the character endChar is found, but not endChar. If character endChar does not exist inside sourceStr then everything is copied over.
 void copyStringUntil(char* sourceStr, char* destinationStr, unsigned int destinationStrSize, char endChar);
 
 //returns the amount of characters until character endChar. If endChar character does not exist in the string then -1 is returned.
