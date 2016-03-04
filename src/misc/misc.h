05ea5bea74d5f03dc315ffe54721bb92e62569e3
MrPlatwnas <platonickola@hotmail.com>
2016-03-04 12:33:03 +0200

    Added argumentsDecode function declaration & comment at misc.h

--- src/misc/misc.h
+++ src/misc/misc.h
@@ -52,4 +52,8 @@ char* getLine();
 
 //decodes the user's command to the command. Returns the command.
 char* commandDecode(char* input_command, char* output_command);
+
+//decodes the user's command to it's arguments. Returns the arguments as a 2D array.
+char** argumentsDecode(char* input_command, unsigned int* arguments_count);
+
 #endif //#ifndef _MISC_H_
