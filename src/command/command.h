b891acdc76f27225e66fc32e8335be9ba36c9f98
MrPlatwnas <platonickola@hotmail.com>
2016-03-04 16:44:39 +0200

    Enhanced visuals at command.h

--- src/command/command.h
+++ src/command/command.h
@@ -22,6 +22,7 @@ int** boardSize(int** grid, unsigned int newSize);
 
 //Clears the board. The two pawns return to their starting position.
 void clear_board(int** grid);
+
 //Sets the number of walls each player has at the start of the game.
 void walls(unsigned int n_walls);
 
