870c20113de33043b6079878584398f7a3953c0d
MrPlatwnas <platonickola@hotmail.com>
2016-03-05 19:43:40 +0200

    Fixed the output message of the list_commands function definition in command.c

--- src/command/command.c
+++ src/command/command.c
@@ -124,11 +124,15 @@ void list_commands()
 {
   static unsigned int n_elems = 13;
   static char* allCommands[] = {"name", "known_command", "list_commands", "quit", "boardsize", "clear_board", "walls", "playmove", "playwall", "genmove", "undo", "winner", "showboard"};
+
+  printf("= ");
   unsigned int counter;
   for(counter = 0; counter < n_elems; counter++)
   {
     printf("%s\n", allCommands[counter]);
   }
+  printf("\n");
+}
 }
 
 int** boardSize(int** grid, unsigned int newSize)
