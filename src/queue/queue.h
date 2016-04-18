aa9e886e08f49fff63c5fc8d51de2477de134611
MrPlatwnas <platonickola@hotmail.com>
2016-04-19 02:22:19 +0300

    queue.c & queue.h: implemented queue data structure to save the grid configurations

---
+++ src/queue/queue.h
@@ -0,0 +1,21 @@
+/******************************************************
+File implementation : queue.c
+Authors             : Platwnas-Nikolaos Kiorpelidis
+Purpose             : queue function declarations
+Date                : 2-1-2015
+*******************************************************/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "../command/command.h"
+
+Queue* stack_construct();
+
+void stack_destruct(Queue* my_queue);
+
+void stack_push(Queue_elem element, Queue* my_queue, ArraySize grid_size);
+
+Queue_elem stack_pop(Queue* my_queue);
+
+#endif /* #ifndef QUEUE_H */
