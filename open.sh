3424f6bd273dae456fdf9999ce4b0e773202316b
Plato Kiorpelidis <platwnace@gmail.com>
2016-09-11 01:53:37 +0300

    open.sh: changed the script to work for all terminals and not only for xfce4-terminal

--- open.sh
+++ open.sh
@@ -1,3 +1,3 @@
 #This file opens all the source files of the project in vim fullscreen, ready to be edited.
 
-xfce4-terminal --fullscreen --command="vim -p src/command/command.cpp src/command/command.hpp src/cli/main.cpp issues_tracker"
+vim -p src/command/command.cpp src/command/command.hpp src/cli/main.cpp issues_tracker
